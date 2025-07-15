754f16f5d0fae7cd0e0ce63c818e9a941d3e1002
Luke Hoffmann <992315+lukehoffmann@users.noreply.github.com>
2025-07-15 12:06:07 +0930

    Refactor script

--- bin/init_todo_file.sh
+++ bin/init_todo_file.sh
@@ -5,16 +5,21 @@ set -o pipefail
 
 date=$1
 
-echo "Setting up To Do for $(date -d "${date}" +"%A, %d %B %Y" )"
+date_yy=`date -d "${date}" +"%Y"`
+date_mm=`date -d "${date}" +"%m"`
+date_yymmdd=`date -d "${date}" -Idate`
+datelong=`date -d "${date}" +"%A, %d %B %Y"`
+
+echo "Setting up To Do for ${datelong}"
 
 root="."
-file="${root}/$(date -d "${date}" +"%Y" )/$(date -d "${date}" +"%m" )/todo-$(date -d "${date}" -Idate).md"
+file="${root}/${date_yy}/${date_mm}/todo-$date_yymmdd.md"
 if [ ! -f $file ]; then
     mkdir -p $(dirname $file)
     echo "Creating file ${file}"
     echo "# To Do:
 
-## $(date -d "${date}" +"%A, %d %B %Y")" > $file
+## ${datelong}" > $file
 fi
 
 echo "Opening file ${file}"
